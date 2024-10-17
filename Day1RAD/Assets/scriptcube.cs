8b2899f12cc5881f3e3e491d69cb7ad203dc372a
STUDENT George Tolkachov <George.Tolkachov@students.ittralee.ie>
2024-10-17 10:55:28 +0100

    Signed-off-by: STUDENT George Tolkachov <George.Tolkachov@students.ittralee.ie>

--- Day1RAD/Assets/scriptcube.cs
+++ Day1RAD/Assets/scriptcube.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class scriptcube : MonoBehaviour
 {
+    float runSpeed = 25f;
+
+    float turnAngle = 135f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,36 @@ public class scriptcube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += runSpeed * transform.forward * Time.deltaTime;
+            transform.Rotate(Vector3.left, 0 * Time.deltaTime);
+            transform.Rotate(Vector3.right, 0 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= runSpeed * transform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turnAngle * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -turnAngle * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += runSpeed * transform.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position -= runSpeed * transform.up * Time.deltaTime;
+        }
     }
 }
